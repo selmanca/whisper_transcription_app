2c1d758df6815ba9f090bbb319bb0176ed136740
selmanca <67283781+selmanca@users.noreply.github.com>
2025-06-01 21:42:33 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
